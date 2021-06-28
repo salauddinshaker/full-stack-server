4eb85ed373b3f6c8451e45e09f6a5b05bd807805
salauddinshaker <sshaker1989@gmail.com>
2021-06-28 14:42:19 +0600

    product added

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require('express')
-const { MongoClient } = require('mongodb');
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 4200
+
+app.use(cors());
+app.use(bodyParser.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8p3b1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const app = express()
-const port = 4200
-
+console.log(process.env.DB_USER);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("dailybazarcollection").collection("ponnoavailable");
+  const ponnoCollection = client.db("dailybazarcollection").collection("ponnoavailable");
   // perform actions on the collection object
   console.log('database connected')
-  app.get('/', (req, res)=>{
-      res.send('OIYeeeeh..........')
+  app.get('/', (req, res) => {
+    res.send('OIYeeeeh..........')
+  })
+
+  app.post('/addProduct', (req, res) => {
+    const newProduct = req.body;
+    ponnoCollection.insertOne(newProduct);
+    console.log('new product added ....', ponnoCollection)
   })
 });
 
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
